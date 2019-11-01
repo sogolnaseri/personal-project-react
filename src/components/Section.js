9155d5fbb78a7ee10bd149b28481e596b9911b1f
Sogol Naseri <snaseri@venacorp.com>
2019-11-01 17:55:14 -0400

    removing helper

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,30 +1,33 @@
 import React from "react";
 import EventDetails from "./EventDetails";
-import { filteredEvents } from "./helper";
+// import { filteredEvents } from "./helper";
 
 export default ({ events, title, type }) => {
   const getEventList = () => {
+    const filteredEvents = () => {
+      if (type === "fork") {
+        return events.filter(event => event.type === "ForkEvent");
+      } else if (type === "pull") {
+        return events.filter(event => event.type === "PullRequestEvent");
+      }
+    };
     if (type === "fork") {
-      return filteredEvents(events, type).map(
-        ({ repo, payload: { forkee } }, i) => (
-          //getEventDetails(forkee.full_name, `Forked From : ${repo.name}`)
-          <EventDetails
-            eventName={forkee.full_name}
-            eventdetails={`Forked From : ${repo.name}`}
-          />
-        )
-      );
+      return filteredEvents().map(({ repo, payload: { forkee } }, i) => (
+        //getEventDetails(forkee.full_name, `Forked From : ${repo.name}`)
+        <EventDetails
+          eventName={forkee.full_name}
+          eventdetails={`Forked From : ${repo.name}`}
+        />
+      ));
     } else if (type === "pull") {
-      return filteredEvents(events, type).map(
-        ({ payload: { pull_request } }, i) => (
-          //getEventDetails(pull_request.title, `Status: ${pull_request.state}`)
-          <EventDetails
-            eventName={pull_request.title}
-            eventdetails={`Status: ${pull_request.state}`}
-            eventUrl={pull_request.html_url}
-          />
-        )
-      );
+      return filteredEvents().map(({ payload: { pull_request } }, i) => (
+        //getEventDetails(pull_request.title, `Status: ${pull_request.state}`)
+        <EventDetails
+          eventName={pull_request.title}
+          eventdetails={`Status: ${pull_request.state}`}
+          eventUrl={pull_request.html_url}
+        />
+      ));
     }
   };
   return (
